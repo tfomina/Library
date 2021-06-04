e76d2431d0657ad9926d796127cce141233da59b
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-06-04 16:37:31 +0300

    Установила пакет @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksController } from './books/books.controller';
 import { BooksService } from './books/books.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, BooksController],
   providers: [AppService, BooksService],
 })

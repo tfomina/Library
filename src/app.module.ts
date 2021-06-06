d6da775a4df879e6cb892b5b36fc89ca20040506
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-06-06 21:58:51 +0300

    Переделала подключение с локальной MongoDB на MongoDB Atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,9 +8,21 @@ import { BooksModule } from './books/books.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot('mongodb://localhost/library_db', {
-      useNewUrlParser: true,
-      useFindAndModify: false,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const userDB = configService.get<string>('DB_USERNAME');
+        const passwordDB = configService.get<string>('DB_PASSWORD');
+        const nameDB = configService.get<string>('DB_NAME');
+
+        return {
+          uri: `mongodb+srv://${userDB}:${passwordDB}@cluster0.m4q9c.mongodb.net/${nameDB}?retryWrites=true&w=majority`,
+          useNewUrlParser: true,
+          useFindAndModify: false,
+          useUnifiedTopology: true,
+        };
+      },
+      inject: [ConfigService],
     }),
     BooksModule,
   ],
